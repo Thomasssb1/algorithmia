060e19f1a4567251f2dbb84a0a07188bc59f4ec8
Thomas Beer <thomas.beer04@outlook.com>
2024-05-30 23:55:32 +0100

    Generate javadoc action

--- lib/src/main/java/com/redshock/algorithmia/maths/Particular.java
+++ lib/src/main/java/com/redshock/algorithmia/maths/Particular.java
@@ -1,17 +1,24 @@
 package com.redshock.algorithmia.maths;
 
+
 /**
  * Holds the particular solution a solved linear diophantine equation
  * The particular solution is the solution
  * x = cs/h and y = ct/h
  * where, as + bt = h, where h = hcf(a,b)
- * @param x x value of the particular solution
- * @param y y value of the particular solution
  */
 public class Particular {
 
     private int x, y;
 
+    /**
+     * Holds the particular solution a solved linear diophantine equation
+     * The particular solution is the solution
+     * x = cs/h and y = ct/h
+     * where, as + bt = h, where h = hcf(a,b)
+     * @param x - x value of the particular solution
+     * @param y - y value of the particular solution
+     */
     public Particular(int x, int y) {
         this.x = x;
         this.y = y;
